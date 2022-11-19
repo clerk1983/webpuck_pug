3a3a4ae4832ceea4b0beafcb2fea6b4b3ecdd71c
CLERK1983 <41065074+clerk1983@users.noreply.github.com>
2022-11-19 21:07:58 +0900

    access.pug

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,63 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-    entry: './src/js/main.js',
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'js/main.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css/,
-                use: [
-                    { loader: MiniCssExtractPlugin.loader, },
-                    { loader: 'css-loader', },
-                ]
-            },
-            {
-                test: /\.(jpg|png)/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            esModule: false,
-                            name: 'img/[name].[ext]'
-                        }
-                    }
-                ]
-            }
+  entry: "./src/js/main.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "js/main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: "css-loader" },
         ],
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename:'./style/main.css',
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/templates/index.html',
-        }),
-        new CleanWebpackPlugin(),
+      },
+      {
+        test: /\.(jpg|png)/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              esModule: false,
+              name: "img/[name].[ext]",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.pug/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+          {
+            loader: "pug-html-loader",
+            options: {
+              pretty: true,
+            },
+          },
+        ],
+      },
     ],
-}
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "./style/main.css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/templates/index.pug",
+      filename: 'index.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/templates/access.pug",
+      filename: 'access.html'
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
